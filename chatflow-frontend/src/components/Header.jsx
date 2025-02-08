952d859caf0eaf5668fd018a4a71bc4db7f3573c
jotam2397 <jmps.10pf4@gmail.com>
2025-02-08 01:33:01 -0300

    Atualizado o package.json com os scripts e dependências do Vite

---
+++ chatflow-frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <header>
+        <h1>Bem-vindo ao Meu Projeto!</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
---
+++ chatflow-frontend/src/pages/src/pages/Home.jsx
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>Bem-vindo ao Meu SaaS!</h1>
+  }
+  
+  export default Home
+  
